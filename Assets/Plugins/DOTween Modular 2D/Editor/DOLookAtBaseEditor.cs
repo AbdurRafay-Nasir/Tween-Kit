aa7f46f17e53a1d501e5e927ffba04979507dd4d
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-01-30 16:00:18 +0500

    DOLookAtBaseEditor resets rotation when tween stops previewing

--- Assets/Plugins/DOTween Modular 2D/Editor/DOLookAtBaseEditor.cs
+++ Assets/Plugins/DOTween Modular 2D/Editor/DOLookAtBaseEditor.cs
@@ -11,14 +11,41 @@ namespace DOTweenModular.Editor
         protected SerializedProperty lookAtTargetProp;
         protected SerializedProperty interpolateProp;
 
+        private DOLookAtBase doLookAt;
+        private string key;
+
         public override void OnEnable()
         {
             base.OnEnable();
+
+            doLookAt = (DOLookAtBase)target;
+            key = "DOLookAtBase_" + doLookAt.gameObject.GetInstanceID();
 
             lookAtProp = serializedObject.FindProperty("lookAt");
             lookAtPositionProp = serializedObject.FindProperty("lookAtPosition");
             lookAtTargetProp = serializedObject.FindProperty("lookAtTarget");
             interpolateProp = serializedObject.FindProperty("interpolate");
+        }
+
+        protected override void OnPreviewStarted()
+        {
+            base.OnPreviewStarted();
+
+            SessionState.SetVector3(key, doLookAt.transform.localEulerAngles);
+        }
+
+        protected override void OnPreviewStopped()
+        {
+            base.OnPreviewStopped();
+
+            doLookAt.transform.localEulerAngles = SessionState.GetVector3(key, doLookAt.transform.localEulerAngles);
+        }
+
+        protected override void OnPreviewForceStopped()
+        {
+            base.OnPreviewForceStopped();
+
+            doLookAt.transform.localEulerAngles = SessionState.GetVector3(key, doLookAt.transform.localEulerAngles);
         }
 
         protected void DrawLookAtSettings()
