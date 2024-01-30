42db0864efca171741c714a2b456b95b8b5f21ae
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-01-30 16:38:12 +0500

    Completed DORotate

--- Assets/Plugins/DOTween Modular 2D/Runtime/DORotate.cs
+++ Assets/Plugins/DOTween Modular 2D/Runtime/DORotate.cs
@@ -1,18 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
-public class DORotate : MonoBehaviour
+namespace DOTweenModular
 {
-    // Start is called before the first frame update
-    void Start()
+    public sealed class DORotate : DOBase
     {
-        
-    }
+        [Tooltip("Fast - Fastest way that never rotates beyond 360° " + "\n" + "\n" +
+         "FastBeyond360 - Fastest way that rotates beyond 360°, use this for full 360° rotation " + "\n" + "\n" +
+         "WorldAxisAdd - Adds the given rotation to the transform using world axis and an advanced precision mode (like when using transform.Rotate(Space.World))" + "\n" +
+         "In this mode the end value is is always considered relative" + "\n" + "\n" +
+         "LocalAxisAdd - Adds the given rotation to the transform's local axis (like when rotating an" + "\n" +
+         "object with the local switch enabled in Unity's editor or using transform.Rotate(Space.Self))" + "\n" +
+         "In this mode the end value is is always considered relative")]
+        public RotateMode rotateMode;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Tooltip("If TRUE, game object will rotate in local space")]
+        public bool useLocal;
+
+        [Tooltip("If TRUE, the Tween will Move duration amount in each second")]
+        public bool speedBased;
+
+        [Tooltip("If TRUE, the targetZRotation will be calculated as: " + "\n" +
+                 "targetZRotation = targetZRotation + transform.rotation")]
+        public bool relative;
+
+        [Tooltip("The rotation to reach, if relative is true game object will rotate as: " + "\n" +
+                 "targetZRotation = targetZRotation + transform.rotation")]
+        public Vector3 targetRotation = new Vector3(90f, 90f, 90f);
+
+        public override Tween CreateTween()
+        {
+            if (useLocal)
+            {
+                Tween = transform.DOLocalRotate(targetRotation, duration, rotateMode);
+            }
+            else
+            {
+                Tween = transform.DORotate(targetRotation, duration, rotateMode);
+            }
+
+            if (easeType == Ease.INTERNAL_Custom)
+                Tween.SetEase(curve);
+            else
+                Tween.SetEase(easeType);
+
+            if (tweenType == Enums.TweenType.Looped)
+                Tween.SetLoops(loops, loopType);
+
+            Tween.SetSpeedBased(speedBased);
+            Tween.SetRelative(relative);
+            Tween.SetDelay(delay);
+
+            TweenCreated();
+
+            return Tween;
+        }
     }
 }
