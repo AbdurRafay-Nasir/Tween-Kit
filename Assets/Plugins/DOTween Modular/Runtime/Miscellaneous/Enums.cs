c8a7e48cc3b60858d939dbcfdab5f7722937e89c
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-02-03 13:00:56 +0500

    Renamed LookAtPath to LookAtAdvanced in Enums.cs

--- Assets/Plugins/DOTween Modular/Runtime/Miscellaneous/Enums.cs
+++ Assets/Plugins/DOTween Modular/Runtime/Miscellaneous/Enums.cs
@@ -29,7 +29,7 @@ namespace DOTweenModular.Enums
         Simple, Looped    
     }
 
-    public enum LookAtPath
+    public enum LookAtAdvanced
     {
         None, Position, Transform, Percentage
     }
