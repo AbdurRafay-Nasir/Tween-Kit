468519d913af5a286fb7d208f32a62c524493059
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-02-04 23:07:38 +0500

    Completed Documentation of DOJump

--- Assets/Plugins/DOTween Modular/Runtime/DOJump.cs
+++ Assets/Plugins/DOTween Modular/Runtime/DOJump.cs
@@ -3,16 +3,28 @@ using DG.Tweening;
 
 namespace DOTweenModular
 {
+    [AddComponentMenu("DOTween Modular/DO Jump")]
     public sealed class DOJump : DOBase
     {
         #region Properties
 
-        public float power;
-        [Min(1)] public int jumpCount;
+        [Tooltip("Power of the jump (the max height of the jump is represented by this plus the final Y offset)")]
+        public float power = 5f;
+
+        [Tooltip("Total number of jumps, atleast 1")]
+        [Min(1)] public int jumpCount = 1;
+
+        [Tooltip("If TRUE the tween will smoothly snap all values to integers")]
         public bool snapping;
+
+        [Tooltip("If TRUE the tween will move in Local Space, regardless of 'relative'")]
         public bool useLocal;
+
+        [Tooltip("If TRUE the tween will move relative to current Position in world space, regardless of 'Use Local'")]
         public bool relative;
-        public Vector3 targetPosition = new Vector3(10f, 10f, 10f);
+
+        [Tooltip("The Position to reach")]
+        public Vector3 targetPosition = new Vector3(1f, 1f, 1f);
 
         #endregion
 
