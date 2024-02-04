63e0b02bf7efcccfdd8d38696a6832603c8c9e3f
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-02-04 23:38:55 +0500

    Documented DORotate

--- Assets/Plugins/DOTween Modular/Runtime/DORotate.cs
+++ Assets/Plugins/DOTween Modular/Runtime/DORotate.cs
@@ -3,26 +3,30 @@ using DG.Tweening;
 
 namespace DOTweenModular
 {
+    [AddComponentMenu("DOTween Modular/DO Rotate")]
     public sealed class DORotate : DOBase
     {
+        #region Properties
+
         [Tooltip("Fast - Fastest way that never rotates beyond 360° " + "\n" + "\n" +
-         "FastBeyond360 - Fastest way that rotates beyond 360°, use this for full 360° rotation " + "\n" + "\n" +
-         "WorldAxisAdd - Adds the given rotation to the transform using world axis and an advanced precision mode (like when using transform.Rotate(Space.World))" + "\n" +
-         "In this mode the end value is is always considered relative" + "\n" + "\n" +
-         "LocalAxisAdd - Adds the given rotation to the transform's local axis (like when rotating an" + "\n" +
-         "object with the local switch enabled in Unity's editor or using transform.Rotate(Space.Self))" + "\n" +
-         "In this mode the end value is is always considered relative")]
+                 "FastBeyond360 - Fastest way that rotates beyond 360°, use this for full 360° rotation " + "\n" + "\n" +
+                 "WorldAxisAdd - Adds the given rotation to the transform using world axis and an advanced precision mode (like when using transform.Rotate(Space.World))" + "\n" +
+                 "In this mode the end value is is always considered relative" + "\n" + "\n" +
+                 "LocalAxisAdd - Adds the given rotation to the transform's local axis (like when rotating an" + "\n" +
+                 "object with the local switch enabled in Unity's editor or using transform.Rotate(Space.Self))" + "\n" +
+                 "In this mode the end value is is always considered relative")]
         public RotateMode rotateMode;
 
-        [Tooltip("If TRUE, game object will rotate in local space")]
+        [Tooltip("If TRUE, gameObject will rotate in local space")]
         public bool useLocal;
 
-        [Tooltip("If TRUE, the Tween will Move duration amount in each second")]
+        [Tooltip("If TRUE, the gameObject will rotate 'duration' amount in each second")]
         public bool speedBased;
 
-        [Tooltip("The rotation to reach, if relative is true game object will rotate as: " + "\n" +
-                 "targetZRotation = targetZRotation + transform.rotation")]
-        public Vector3 targetRotation = new Vector3(90f, 90f, 90f);
+        [Tooltip("The rotation to reach")]
+        public Vector3 targetRotation = new(90f, 90f, 90f);
+
+        #endregion
 
         public override Tween CreateTween()
         {
