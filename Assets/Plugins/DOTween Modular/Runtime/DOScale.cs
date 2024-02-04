37a11f86b8f97f9a3b052cb97d59ef7d7fce50d5
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-02-04 23:39:03 +0500

    Documented DOScale

--- Assets/Plugins/DOTween Modular/Runtime/DOScale.cs
+++ Assets/Plugins/DOTween Modular/Runtime/DOScale.cs
@@ -3,25 +3,26 @@ using DG.Tweening;
 
 namespace DOTweenModular
 {
+    [AddComponentMenu("DOTween Modular/DO Scale")]
     public sealed class DOScale : DOLookAtBase
     {
         #region Properties
 
-        [Tooltip("If TRUE, the tween will be scaled 'duration' amount in each second")]
+        [Tooltip("If TRUE, the gameObject will be scaled 'duration' amount in each second")]
         public bool speedBased;
 
         [Tooltip("If TRUE, the targetScale will be calculated as: " + "\n" +
-                "targetScale = targetScale + transform.localScale")]
+                 "targetScale = targetScale + transform.localScale")]
         public bool relative;
 
-        [Tooltip("The position to reach, if relative is true game object will move as: " + "\n" +
-                 "targetScale = targetScale + transform.localScale")]
-        public Vector3 targetScale = new Vector3(3f, 3f, 3f);
+        [Tooltip("The scale to reach")]
+        public Vector3 targetScale = new(3f, 3f, 3f);
 
         #endregion
 
         public override Tween CreateTween()
         {
+            // DOScale works on local scale
             Tween = transform.DOScale(targetScale, duration);
 
             if (easeType == Ease.INTERNAL_Custom)
