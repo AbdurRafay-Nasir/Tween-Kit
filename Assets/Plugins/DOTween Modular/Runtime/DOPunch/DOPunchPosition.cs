f8ba90716a20359cfee6bc20bf3f8d48826b6fd9
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-02-05 13:40:33 +0500

    Documented DOPunchPosition

--- Assets/Plugins/DOTween Modular/Runtime/DOPunch/DOPunchPosition.cs
+++ Assets/Plugins/DOTween Modular/Runtime/DOPunch/DOPunchPosition.cs
@@ -2,8 +2,10 @@ using DG.Tweening;
 
 namespace DOTweenModular
 {
+    [UnityEngine.AddComponentMenu("DOTween Modular/DO Punch Position")]
     public sealed class DOPunchPosition : DOPunchBase
     {
+        [UnityEngine.Tooltip("If TRUE the tween will smoothly snap all values to integers")]
         public bool snapping;
 
         protected override void InitializeTween()
