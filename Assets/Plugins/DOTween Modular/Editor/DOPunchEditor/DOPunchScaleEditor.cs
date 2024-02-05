f53703a81612e2a6b40f9f63f6bb59d239bf27f9
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-02-05 22:38:59 +0500

    Removed Saving/loading of saved Scale in DOPunchScaleEditor

--- Assets/Plugins/DOTween Modular/Editor/DOPunchEditor/DOPunchScaleEditor.cs
+++ Assets/Plugins/DOTween Modular/Editor/DOPunchEditor/DOPunchScaleEditor.cs
@@ -7,41 +7,12 @@ namespace DOTweenModular.Editor
     [CustomEditor(typeof(DOPunchScale)), CanEditMultipleObjects]
     public sealed class DOPunchScaleEditor : DOPunchBaseEditor
     {
-        private DOPunchScale doPunchScale;
-        private string scaleKey;
-
-        public override void OnEnable()
-        {
-            base.OnEnable();
-
-            doPunchScale = (DOPunchScale)target;
-            scaleKey = "DOPunchScale_" + instanceId;
-        }
-
         protected override void DrawValues()
         {
             DrawProperty(punchProp);
 
             base.DrawValues();
         }
-
-        #region Tween Preview Functions
-
-        protected override void OnPreviewStarted()
-        {
-            base.OnPreviewStarted();
-
-            SessionState.SetVector3(scaleKey, doPunchScale.transform.localScale);
-        }
-
-        protected override void OnPreviewStopped()
-        {
-            base.OnPreviewStopped();
-
-            doPunchScale.transform.localScale = SessionState.GetVector3(scaleKey, doPunchScale.transform.localScale);
-        }
-
-        #endregion
     }
 }
 
