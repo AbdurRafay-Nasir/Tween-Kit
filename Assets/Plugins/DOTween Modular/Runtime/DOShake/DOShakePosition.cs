79cc7986939bf8434cd1d7e1e85386afbc09cc59
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-02-05 13:32:31 +0500

    Documented DOShakePosition

--- Assets/Plugins/DOTween Modular/Runtime/DOShake/DOShakePosition.cs
+++ Assets/Plugins/DOTween Modular/Runtime/DOShake/DOShakePosition.cs
@@ -3,8 +3,10 @@ using DG.Tweening;
 
 namespace DOTweenModular
 {
+    [AddComponentMenu("DOTween Modular/DO Shake Position")]
     public sealed class DOShakePosition : DOShakeBase
     {
+        [Tooltip("If TRUE the tween will smoothly snap all values to integers")]
         public bool snapping;
 
         protected override void InitializeTween()
