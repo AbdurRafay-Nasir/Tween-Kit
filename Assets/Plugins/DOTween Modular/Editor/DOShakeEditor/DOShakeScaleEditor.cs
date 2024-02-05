b85e724cb02019b25b3db090d29c7f3db8ee21b4
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-02-05 22:31:35 +0500

    Removed Saving/loading of saved Scale in DOShakeScaleEditor

--- Assets/Plugins/DOTween Modular/Editor/DOShakeEditor/DOShakeScaleEditor.cs
+++ Assets/Plugins/DOTween Modular/Editor/DOShakeEditor/DOShakeScaleEditor.cs
@@ -7,42 +7,12 @@ namespace DOTweenModular.Editor
     [CustomEditor(typeof(DOShakeScale)), CanEditMultipleObjects]
     public sealed class DOShakeScaleEditor : DOShakeBaseEditor
     {
-        private DOShakeScale doShakeScale;
-        private string scaleKey;
-
-        public override void OnEnable()
-        {
-            base.OnEnable();
-
-            doShakeScale = (DOShakeScale)target;
-            scaleKey = "DOShakeScale_" + instanceId;
-        }
-
         protected override void DrawValues()
         {
             DrawProperty(strengthProp);
 
             base.DrawValues();
         }
-
-        #region Tween Preview Functions
-
-        protected override void OnPreviewStarted()
-        {
-            base.OnPreviewStarted();
-
-            SessionState.SetVector3(scaleKey, doShakeScale.transform.localScale);
-        }
-
-        protected override void OnPreviewStopped()
-        {
-            base.OnPreviewStopped();
-
-            doShakeScale.transform.localScale = SessionState.GetVector3(scaleKey, doShakeScale.transform.localScale);
-        }
-
-        #endregion
-
     }
 }
 
