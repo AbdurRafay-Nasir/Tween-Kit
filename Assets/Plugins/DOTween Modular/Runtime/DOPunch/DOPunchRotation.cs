665b46c04d54718d74f519c902dd4cf842c4442a
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-02-05 13:41:47 +0500

    Documented DOPunchRotation

--- Assets/Plugins/DOTween Modular/Runtime/DOPunch/DOPunchRotation.cs
+++ Assets/Plugins/DOTween Modular/Runtime/DOPunch/DOPunchRotation.cs
@@ -2,6 +2,7 @@ using DG.Tweening;
 
 namespace DOTweenModular
 {
+    [UnityEngine.AddComponentMenu("DOTween Modular/DO Punch Rotation")]
     public sealed class DOPunchRotation : DOPunchBase
     {
         protected override void InitializeTween()
