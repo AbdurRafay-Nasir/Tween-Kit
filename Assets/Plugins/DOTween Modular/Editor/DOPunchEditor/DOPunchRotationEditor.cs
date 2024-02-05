d331ecf291c168b1e6025db950bb8584f28e94da
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-02-05 22:37:56 +0500

    Removed Saving/loading of saved Rotation in DOPunchRotationEditor

--- Assets/Plugins/DOTween Modular/Editor/DOPunchEditor/DOPunchRotationEditor.cs
+++ Assets/Plugins/DOTween Modular/Editor/DOPunchEditor/DOPunchRotationEditor.cs
@@ -7,41 +7,12 @@ namespace DOTweenModular.Editor
     [CustomEditor(typeof(DOPunchRotation)), CanEditMultipleObjects]
     public sealed class DOPunchRotationEditor : DOPunchBaseEditor
     {
-        private DOPunchRotation doPunchRotation;
-        private string rotationKey;
-
-        public override void OnEnable()
-        {
-            base.OnEnable();
-
-            doPunchRotation = (DOPunchRotation)target;
-            rotationKey = "DOPunchRotation_" + instanceId;
-        }
-
         protected override void DrawValues()
         {
             DrawProperty(punchProp);
 
             base.DrawValues();
         }
-
-        #region Tween Preview Functions
-
-        protected override void OnPreviewStarted()
-        {
-            base.OnPreviewStarted();
-
-            SessionState.SetVector3(rotationKey, doPunchRotation.transform.eulerAngles);
-        }
-
-        protected override void OnPreviewStopped()
-        {
-            base.OnPreviewStopped();
-
-            doPunchRotation.transform.eulerAngles = SessionState.GetVector3(rotationKey, doPunchRotation.transform.eulerAngles);
-        }
-
-        #endregion
     }
 }
 
