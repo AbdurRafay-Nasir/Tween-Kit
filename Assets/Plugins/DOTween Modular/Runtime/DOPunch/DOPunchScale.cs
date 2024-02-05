8219a45902327d17fb977374b0d3918761643bcc
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-02-05 13:42:34 +0500

    Documented DOPunchScale

--- Assets/Plugins/DOTween Modular/Runtime/DOPunch/DOPunchScale.cs
+++ Assets/Plugins/DOTween Modular/Runtime/DOPunch/DOPunchScale.cs
@@ -2,6 +2,7 @@ using DG.Tweening;
 
 namespace DOTweenModular
 {
+    [UnityEngine.AddComponentMenu("DOTween Modular/DO Punch Scale")]
     public sealed class DOPunchScale : DOPunchBase
     {
         protected override void InitializeTween()
