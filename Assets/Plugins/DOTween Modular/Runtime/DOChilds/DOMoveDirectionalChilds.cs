f5df4062941fc5256ac6c18f9893c9fe81a78588
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-02-09 22:34:47 +0500

    Completed DOMoveDirectionalChilds

---
+++ Assets/Plugins/DOTween Modular/Runtime/DOChilds/DOMoveDirectionalChilds.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using DG.Tweening;
+using DOTweenModular.Enums;
+
+namespace DOTweenModular
+{
+    [AddComponentMenu("DOTween Modular/DO Move Directional Childs")]
+    public sealed class DOMoveDirectionalChilds : DOBase
+    {
+        #region Properties
+
+        [Tooltip("If TRUE, All childs will move simultaneously")]
+        public bool join;
+
+        [Tooltip("The direction to move in")]
+        public Direction direction;
+
+        [Tooltip("If TRUE, the tween will smoothly snap all values to integers")]
+        public bool snapping;
+
+        [Tooltip("The amount to move in direction defined by 'Direction' property")]
+        public float moveAmount = 5f;
+
+        #endregion
+
+        public override Tween CreateTween()
+        {
+            Sequence childsMoveSequence = DOTween.Sequence();
+
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Transform child = transform.GetChild(i);
+
+                Vector3 childMoveDirection = Vector3.zero;
+
+                switch (direction)
+                {
+                    case Direction.LocalUp:
+                        childMoveDirection = child.TransformDirection(Vector3.up);
+                        break;
+                    case Direction.LocalRight:
+                        childMoveDirection = child.TransformDirection(Vector3.right);
+                        break;
+                    case Direction.LocalForward:
+                        childMoveDirection = child.TransformDirection(Vector3.forward);
+                        break;
+                }
+
+                Tween childMoveTween = child.DOMove(child.position + childMoveDirection * moveAmount, 
+                                                    duration, snapping);
+
+                if (easeType == Ease.INTERNAL_Custom)
+                    childMoveTween.SetEase(curve);
+                else
+                    childMoveTween.SetEase(easeType);
+
+                childMoveTween.SetDelay(delay);
+
+                if (join)
+                    childsMoveSequence.Join(childMoveTween);
+                else
+                    childsMoveSequence.Append(childMoveTween);
+            }
+
+            if (tweenType == Enums.TweenType.Looped)
+                childsMoveSequence.SetLoops(loops, loopType);
+
+            Tween = childsMoveSequence;
+
+            TweenCreated();
+
+            return Tween;
+        }
+    }
+}
