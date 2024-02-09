77949ab458ebadfb753497b3254ee47887657de0
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-02-09 20:55:23 +0500

    Completed DORotateChildsEditor

---
+++ Assets/Plugins/DOTween Modular/Editor/DORotateChildsEditor.cs
@@ -0,0 +1,169 @@
+#if UNITY_EDITOR
+
+using UnityEditor;
+
+namespace DOTweenModular.Editor
+{
+    [CustomEditor(typeof(DORotateChilds)), CanEditMultipleObjects]
+    public sealed class DORotateChildsEditor : DOBaseEditor
+    {
+        #region Serialized Properties
+
+        private SerializedProperty joinProp;
+        private SerializedProperty rotateModeProp;
+        private SerializedProperty useLocalProp;
+        private SerializedProperty targetRotationProp;
+
+        #endregion
+
+        #region Unity Functions
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+
+            joinProp = serializedObject.FindProperty("join");
+            rotateModeProp = serializedObject.FindProperty("rotateMode");
+            useLocalProp = serializedObject.FindProperty("useLocal");
+            targetRotationProp = serializedObject.FindProperty("targetRotation");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            Space();
+
+            bool[] toggleStates = DrawToggles("Life", "Type", "Childs", "Values", "Events");
+
+            Space();
+
+            if (toggleStates[0])
+            {
+                DrawSeparatorLine();
+
+                if (BeginFoldout("Life Time Settings"))
+                {
+                    EditorGUI.indentLevel++;
+
+                    BeginBackgroundBox();
+                    Space();
+
+                    DrawLifeTimeSettings();
+
+                    Space();
+                    EndBackgroundBox();
+
+                    EditorGUI.indentLevel--;
+                }
+
+                EndFoldout();
+            }
+
+            DrawTweenObjectHelpBox();
+
+            if (toggleStates[1])
+            {
+                DrawSeparatorLine();
+
+                if (BeginFoldout("Type Settings"))
+                {
+                    EditorGUI.indentLevel++;
+
+                    BeginBackgroundBox();
+                    Space();
+
+                    DrawTypeSettings();
+
+                    Space();
+                    EndBackgroundBox();
+
+                    EditorGUI.indentLevel--;
+                }
+
+                EndFoldout();
+            }
+
+            if (toggleStates[2])
+            {
+                DrawSeparatorLine();
+
+                if (BeginFoldout("Childs Rotate Settings"))
+                {
+                    EditorGUI.indentLevel++;
+
+                    BeginBackgroundBox();
+                    Space();
+
+                    DrawChildsRotateSettings();
+
+                    Space();
+                    EndBackgroundBox();
+
+                    EditorGUI.indentLevel--;
+                }
+
+                EndFoldout();
+            }
+
+            if (toggleStates[3])
+            {
+                DrawSeparatorLine();
+
+                if (BeginFoldout("Values"))
+                {
+                    EditorGUI.indentLevel++;
+
+                    BeginBackgroundBox();
+                    Space();
+
+                    DrawProperty(targetRotationProp);
+                    DrawValues();
+
+                    Space();
+                    EndBackgroundBox();
+
+                    EditorGUI.indentLevel--;
+                }
+
+                EndFoldout();
+            }
+
+            if (toggleStates[4])
+            {
+                DrawSeparatorLine();
+
+                if (BeginFoldout("Events", false))
+                {
+                    EditorGUI.indentLevel++;
+
+                    Space();
+
+                    DrawEvents();
+
+                    EditorGUI.indentLevel--;
+                }
+                EndFoldout();
+            }
+
+            DrawSeparatorLine();
+
+            DrawPlayButton();
+            DrawStopButton();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Draws Rotate Mode, Speed Based and use Local properties
+        /// </summary>
+        private void DrawChildsRotateSettings()
+        {
+            DrawProperty(joinProp);
+            DrawProperty(rotateModeProp);
+            DrawProperty(useLocalProp);
+        }
+    }
+}
+
+#endif
