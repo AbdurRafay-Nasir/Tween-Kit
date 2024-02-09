6418e4e0ace8f244bfebb44ba648a762aadad816
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-02-09 18:04:19 +0500

    Completed DOScaleChildsEditor

---
+++ Assets/Plugins/DOTween Modular/Editor/DOScaleChildsEditor.cs
@@ -0,0 +1,201 @@
+#if UNITY_EDITOR
+
+using UnityEditor;
+
+namespace DOTweenModular.Editor
+{
+    [CustomEditor(typeof(DOScaleChilds)), CanEditMultipleObjects]
+    public class DOScaleChildsEditor : DOBaseEditor
+    {
+        #region Serialized Properties
+
+        private SerializedProperty joinProp;
+        private SerializedProperty relativeProp;
+        private SerializedProperty targetScaleProp;
+
+        #endregion
+
+        private DOScaleChilds doScaleChilds;
+        private RelativeFlags relativeFlags;
+
+        #region Unity Functions
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+
+            doScaleChilds = (DOScaleChilds)target;
+            relativeFlags = CreateInstance<RelativeFlags>();
+
+            joinProp = serializedObject.FindProperty("join");
+            relativeProp = serializedObject.FindProperty("relative");
+            targetScaleProp = serializedObject.FindProperty("targetScale");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            Space();
+
+            bool[] toggleStates = DrawToggles("Life", "Type", "Childs", "Values", "Events");
+
+            Space();
+
+            if (toggleStates[0])
+            {
+                DrawSeparatorLine();
+
+                if (BeginFoldout("Life Time Settings"))
+                {
+                    EditorGUI.indentLevel++;
+
+                    BeginBackgroundBox();
+                    Space();
+
+                    DrawLifeTimeSettings();
+
+                    Space();
+                    EndBackgroundBox();
+
+                    EditorGUI.indentLevel--;
+                }
+
+                EndFoldout();
+            }
+
+            DrawTweenObjectHelpBox();
+
+            if (toggleStates[1])
+            {
+                DrawSeparatorLine();
+
+                if (BeginFoldout("Type Settings"))
+                {
+                    EditorGUI.indentLevel++;
+
+                    BeginBackgroundBox();
+                    Space();
+
+                    DrawTypeSettings();
+
+                    Space();
+                    EndBackgroundBox();
+
+                    EditorGUI.indentLevel--;
+                }
+
+                EndFoldout();
+            }
+
+            if (toggleStates[2])
+            {
+                DrawSeparatorLine();
+
+                if (BeginFoldout("Childs Scale Settings"))
+                {
+                    EditorGUI.indentLevel++;
+
+                    BeginBackgroundBox();
+                    Space();
+
+                    DrawChildScaleSettings();
+                    SetTargetScale();
+
+                    Space();
+                    EndBackgroundBox();
+
+                    EditorGUI.indentLevel--;
+                }
+
+                EndFoldout();
+            }
+
+            if (toggleStates[3])
+            {
+                DrawSeparatorLine();
+
+                if (BeginFoldout("Values"))
+                {
+                    EditorGUI.indentLevel++;
+
+                    BeginBackgroundBox();
+                    Space();
+
+                    DrawProperty(targetScaleProp);
+                    DrawValues();
+
+                    Space();
+                    EndBackgroundBox();
+
+                    EditorGUI.indentLevel--;
+                }
+
+                EndFoldout();
+            }
+
+            if (toggleStates[4])
+            {
+                DrawSeparatorLine();
+
+                if (BeginFoldout("Events", false))
+                {
+                    EditorGUI.indentLevel++;
+
+                    Space();
+
+                    DrawEvents();
+
+                    EditorGUI.indentLevel--;
+                }
+                EndFoldout();
+            }
+
+            DrawSeparatorLine();
+
+            DrawPlayButton();
+            DrawStopButton();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        #endregion
+
+        private void DrawChildScaleSettings()
+        {
+            DrawProperty(joinProp);
+            DrawProperty(relativeProp);
+        }
+
+        /// <summary>
+        /// Update Target Scale when switching from relative/absolute modes
+        /// </summary>
+        private void SetTargetScale()
+        {
+            if (doScaleChilds.relative)
+            {
+                if (relativeFlags.firstTimeRelative)
+                {
+                    doScaleChilds.targetScale -= doScaleChilds.transform.localScale;
+
+                    Undo.RecordObject(relativeFlags, "DOScaleChildsEditor_firstTimeNonRelative");
+                    relativeFlags.firstTimeRelative = false;
+                }
+
+                relativeFlags.firstTimeNonRelative = true;
+            }
+            else
+            {
+                if (relativeFlags.firstTimeNonRelative)
+                {
+                    doScaleChilds.targetScale += doScaleChilds.transform.localScale;
+
+                    Undo.RecordObject(relativeFlags, "DOScaleChildsChildsEditor_firstTimeRelative");
+                    relativeFlags.firstTimeNonRelative = false;
+                }
+
+                relativeFlags.firstTimeRelative = true;
+            }
+        }
+    }
+}
+
+#endif
