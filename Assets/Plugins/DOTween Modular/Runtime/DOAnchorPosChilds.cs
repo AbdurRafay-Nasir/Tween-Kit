0ceb7707768000f4b3fd7b2afd47db82c4e288d2
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-02-09 17:49:11 +0500

    Completed DOAnchorPosChilds

---
+++ Assets/Plugins/DOTween Modular/Runtime/DOAnchorPosChilds.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using DG.Tweening;
+
+namespace DOTweenModular
+{
+    [AddComponentMenu("DOTween Modular/DO Anchor Pos Childs")]
+    public sealed class DOAnchorPosChilds : DOBase
+    {
+        #region Properties
+
+        [Tooltip("If TRUE, All childs will move simultaneously")]
+        public bool join;
+
+        [Tooltip("If TRUE the childs will move relative to their current Position in world space" + "\n" +
+                 "Target Position for each child will be calculated as: " + "\n" +
+                 "Target Position = Target Position + child current position")]
+        public bool relative;
+
+        [Tooltip("If TRUE, the tween will smoothly snap all values to integers")]
+        public bool snapping;
+
+        [Tooltip("The position to reach")]
+        public Vector2 targetPosition;
+
+        #endregion
+
+        public override Tween CreateTween()
+        {
+            Sequence childsAnchorPosSequence = DOTween.Sequence();
+
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                RectTransform childRectTransform = (RectTransform)transform.GetChild(i);
+
+                Tween childMoveTween = childRectTransform.DOAnchorPos(targetPosition, duration, snapping);
+
+                if (easeType == Ease.INTERNAL_Custom)
+                    childMoveTween.SetEase(curve);
+                else
+                    childMoveTween.SetEase(easeType);
+
+                childMoveTween.SetRelative(relative);
+                childMoveTween.SetDelay(delay);
+
+                if (join)
+                    childsAnchorPosSequence.Join(childMoveTween);
+                else
+                    childsAnchorPosSequence.Append(childMoveTween);
+            }
+
+            if (tweenType == Enums.TweenType.Looped)
+                childsAnchorPosSequence.SetLoops(loops, loopType);
+
+            Tween = childsAnchorPosSequence;
+
+            TweenCreated();
+
+            return Tween;
+        }
+    }
+}
