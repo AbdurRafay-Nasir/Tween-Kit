9f9b3c34f05b4761b7a8ea77b7c2f8bd3c6eb47a
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-02-09 21:08:20 +0500

    Removed Tooltips from Begin Enum in Enums.cs

--- Assets/Plugins/DOTween Modular/Runtime/Miscellaneous/Enums.cs
+++ Assets/Plugins/DOTween Modular/Runtime/Miscellaneous/Enums.cs
@@ -3,25 +3,8 @@ using UnityEngine;
 namespace DOTweenModular.Enums
 {
     public enum Begin
-    {
-        [Tooltip("Start as soon game runs")]
-        OnSceneStart,
-
-        [Tooltip("Start only When visible for first time")]
-        OnVisible,
-
-        [Tooltip("Start when something enters the trigger")]
-        OnTrigger,
-
-        [Tooltip("Will not start on its own, you have to either manually call CreateTween().PlayTween()" + "\n" +
-                 "or use this componenet in a sequence component")]
-        Manual,
-    
-        [Tooltip("Start AFTER Tween Object's tween is completed")]
-        After,
-
-        [Tooltip("Start when Tween Object's tween is created")]
-        With
+    { 
+        OnSceneStart, OnVisible, OnTrigger, Manual, After, With
     }
 
     public enum TweenType
