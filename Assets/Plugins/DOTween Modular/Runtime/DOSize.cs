c46fba3811edaa104748bfadac8c1eef8bd6898d
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-02-12 16:53:55 +0500

    Completed DOSize

---
+++ Assets/Plugins/DOTween Modular/Runtime/DOSize.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using DG.Tweening;
+using DOTweenModular.Miscellaneous;
+
+namespace DOTweenModular
+{
+    [AddComponentMenu("DOTween Modular/DO Size")]
+    public sealed class DOSize : DOBase
+    {
+        #region Properties
+
+        [Tooltip("If TRUE, the tween will Move 'duration' amount in each second")]
+        public bool speedBased;
+
+        [Tooltip("If TRUE, the targetSize will be calculated as: " + "\n" +
+                  "targetSize = targetSize + spriteRenderer.size")]
+        public bool relative;
+
+        [Tooltip("The size to reach, if relative is true Sprite Rendere size will tween as: " + "\n" +
+                 "targetSize = targetSize + spriteRenderer.size")]
+        public Vector2 targetSize = new(3f, 3f);
+
+        #endregion
+
+        public override Tween CreateTween()
+        {
+            SpriteRenderer sr = GetComponent<SpriteRenderer>();
+            Tween = sr.DOSize(targetSize, duration);
+
+            if (easeType == Ease.INTERNAL_Custom)
+                Tween.SetEase(curve);
+            else
+                Tween.SetEase(easeType);
+
+            if (tweenType == Enums.TweenType.Looped)
+                Tween.SetLoops(loops, loopType);
+
+            Tween.SetSpeedBased(speedBased);
+            Tween.SetRelative(relative);
+            Tween.SetDelay(delay);
+
+            TweenCreated();
+
+            return Tween;
+        }
+    }
+}
