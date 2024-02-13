4d5540aa1880af599316873a2e83b1c9ec073f23
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-02-13 18:08:49 +0500

    DOScaleChilds now inherits DOChildsBase

--- Assets/Plugins/DOTween Modular/Runtime/DOChilds/DOScaleChilds.cs
+++ Assets/Plugins/DOTween Modular/Runtime/DOChilds/DOScaleChilds.cs
@@ -4,12 +4,9 @@ using DG.Tweening;
 namespace DOTweenModular
 {
     [AddComponentMenu("DOTween Modular/DO Scale Childs")]
-    public sealed class DOScaleChilds : DOBase
+    public sealed class DOScaleChilds : DOChildsBase
     {
         #region Properties
-
-        [Tooltip("If TRUE, All childs will move simultaneously")]
-        public bool join = true;
 
         [Tooltip("If TRUE the childs will Scale relative to their current Local Scale" + "\n" +
                  "Target Scale for each child will be calculated as: " + "\n" +
@@ -17,40 +14,14 @@ namespace DOTweenModular
         public bool relative;
 
         [Tooltip("The scale to reach")]
-        public Vector3 targetScale = new Vector3(3f, 3f, 3f);
+        public Vector3 targetScale = new(3f, 3f, 3f);
 
         #endregion
 
-        public override Tween CreateTween()
+        protected override Tween InitializeChildTween(Transform currentChild)
         {
-            Sequence childsScaleSequence = DOTween.Sequence();
-
-            for (int i = 0; i < transform.childCount; i++)
-            {
-                Tween childMoveTween = transform.GetChild(i).DOScale(targetScale, duration);
-
-                if (easeType == Ease.INTERNAL_Custom)
-                    childMoveTween.SetEase(curve);
-                else
-                    childMoveTween.SetEase(easeType);
-
-                childMoveTween.SetRelative(relative);
-                childMoveTween.SetDelay(delay);
-
-                if (join)
-                    childsScaleSequence.Join(childMoveTween);
-                else
-                    childsScaleSequence.Append(childMoveTween);
-            }
-
-            if (tweenType == Enums.TweenType.Looped)
-                childsScaleSequence.SetLoops(loops, loopType);
-
-            Tween = childsScaleSequence;
-
-            TweenCreated();
-
-            return Tween;
+            return currentChild.DOScale(targetScale, duration)
+                               .SetRelative(relative);            
         }
     }
 }
