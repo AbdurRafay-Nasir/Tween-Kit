82ae955e2ef82a46256915af90eeddd886380904
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-02-13 23:26:27 +0500

    Completed DOSizeChilds

---
+++ Assets/Plugins/DOTween Modular/Runtime/DOChilds/DOSizeChilds.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using DG.Tweening;
+using DOTweenModular.Miscellaneous;
+
+namespace DOTweenModular
+{
+    [AddComponentMenu("DOTween Modular/DO Size Childs")]
+    public sealed class DOSizeChilds : DOChildsBase
+    {
+        #region Properties
+
+        [Tooltip("If TRUE, the targetSize will be calculated as: " + "\n" +
+          "targetSize = targetSize + spriteRenderer.size")]
+        public bool relative;
+
+        [Tooltip("The size to reach")]
+        public Vector2 targetSize = new(3f, 3f);
+
+        #endregion
+
+        protected override Tween InitializeChildTween(Transform currentChild)
+        {
+            SpriteRenderer sr = currentChild.GetComponent<SpriteRenderer>();
+
+            return sr.DOSize(targetSize, duration).SetRelative(relative);
+        }
+    }
+}
