ee1df7fa8a83552ee7788695fe8167d1c9e49287
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-02-13 23:26:09 +0500

    Added Functionality to create scripts using new script templates
    in CreateDOComponentFromScript

--- Assets/Plugins/DOTween Modular/Editor/Script Template/CreateDOComponentFromScript.cs
+++ Assets/Plugins/DOTween Modular/Editor/Script Template/CreateDOComponentFromScript.cs
@@ -12,12 +12,28 @@ public static class CreateDOComponentFromScript
         ProjectWindowUtil.CreateScriptAssetFromTemplateFile(templatePath, "DOComponent.cs");
     }
 
+    [MenuItem("Assets/Create/DOTween Modular/DOChildsComponent", priority = 20)]
+    public static void CreateDoChildsComponent()
+    {
+        string templatePath = "Assets/Plugins/DOTween Modular/Editor/Script Template/DOChildsComponent.cs.txt";
+
+        ProjectWindowUtil.CreateScriptAssetFromTemplateFile(templatePath, "DOChildsComponent.cs");
+    }
+
     [MenuItem("Assets/Create/DOTween Modular/DOComponentEditor")]
     public static void CreateDoComponentEditor()
     {
         string templatePath = "Assets/Plugins/DOTween Modular/Editor/Script Template/DOComponentEditor.cs.txt";
 
         ProjectWindowUtil.CreateScriptAssetFromTemplateFile(templatePath, "DOComponentEditor.cs");
+    }
+
+    [MenuItem("Assets/Create/DOTween Modular/DOChildsComponentEditor")]
+    public static void CreateDoChildsComponentEditor()
+    {
+        string templatePath = "Assets/Plugins/DOTween Modular/Editor/Script Template/DOChildsComponentEditor.cs.txt";
+
+        ProjectWindowUtil.CreateScriptAssetFromTemplateFile(templatePath, "DOChildsComponentEditor.cs");
     }
 }
 
