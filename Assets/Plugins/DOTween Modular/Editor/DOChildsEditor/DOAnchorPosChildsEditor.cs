c8d7131fc8717aa81c0c9ade80dffe7ee59fcb49
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-02-14 22:11:35 +0500

    DOAcnhorPosChildsEditor now displays error if no childs are found

--- Assets/Plugins/DOTween Modular/Editor/DOChildsEditor/DOAnchorPosChildsEditor.cs
+++ Assets/Plugins/DOTween Modular/Editor/DOChildsEditor/DOAnchorPosChildsEditor.cs
@@ -15,11 +15,15 @@ namespace DOTweenModular.Editor
 
         #endregion
 
+        private UnityEngine.Transform transform;
+
         #region Unity Functions
 
         public override void OnEnable()
         {
             base.OnEnable();
+
+            transform = ((DOAnchorPosChilds)target).transform;
 
             relativeProp = serializedObject.FindProperty("relative");
             snappingProp = serializedObject.FindProperty("snapping");
@@ -28,6 +32,13 @@ namespace DOTweenModular.Editor
 
         public override void OnInspectorGUI()
         {
+            if (transform.childCount == 0)
+            {
+                DrawHelpbox("There are no Child Game Objects, What are you supposed to do with this Component?", MessageType.Error);
+
+                return;
+            }
+
             Space();
 
             bool[] toggleStates = DrawToggles("Life", "Type", "Child", "Values", "Events");
