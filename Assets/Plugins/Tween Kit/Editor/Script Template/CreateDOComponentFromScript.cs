c25c2b860935c498b603b0d174d19aede38ca8f3
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-02-18 23:49:35 +0500

    Changed DOTween Modular to Tween Kit in CreateDOComponentFromScript

--- Assets/Plugins/Tween Kit/Editor/Script Template/CreateDOComponentFromScript.cs
+++ Assets/Plugins/Tween Kit/Editor/Script Template/CreateDOComponentFromScript.cs
@@ -4,34 +4,34 @@ using UnityEditor;
 
 public static class CreateDOComponentFromScript
 {
-    [MenuItem("Assets/Create/DOTween Modular/DOComponent", priority = 20)]
+    [MenuItem("Assets/Create/Tween Kit/DOComponent", priority = 20)]
     public static void CreateDoComponent()
     {
-        string templatePath = "Assets/Plugins/DOTween Modular/Editor/Script Template/DOComponent.cs.txt";
+        string templatePath = "Assets/Plugins/Tween Kit/Editor/Script Template/DOComponent.cs.txt";
 
         ProjectWindowUtil.CreateScriptAssetFromTemplateFile(templatePath, "DOComponent.cs");
     }
 
-    [MenuItem("Assets/Create/DOTween Modular/DOChildsComponent", priority = 20)]
+    [MenuItem("Assets/Create/Tween Kit/DOChildsComponent", priority = 20)]
     public static void CreateDoChildsComponent()
     {
-        string templatePath = "Assets/Plugins/DOTween Modular/Editor/Script Template/DOChildsComponent.cs.txt";
+        string templatePath = "Assets/Plugins/Tween Kit/Editor/Script Template/DOChildsComponent.cs.txt";
 
         ProjectWindowUtil.CreateScriptAssetFromTemplateFile(templatePath, "DOChildsComponent.cs");
     }
 
-    [MenuItem("Assets/Create/DOTween Modular/DOComponentEditor")]
+    [MenuItem("Assets/Create/Tween Kit/DOComponentEditor")]
     public static void CreateDoComponentEditor()
     {
-        string templatePath = "Assets/Plugins/DOTween Modular/Editor/Script Template/DOComponentEditor.cs.txt";
+        string templatePath = "Assets/Plugins/Tween Kit/Editor/Script Template/DOComponentEditor.cs.txt";
 
         ProjectWindowUtil.CreateScriptAssetFromTemplateFile(templatePath, "DOComponentEditor.cs");
     }
 
-    [MenuItem("Assets/Create/DOTween Modular/DOChildsComponentEditor")]
+    [MenuItem("Assets/Create/Tween Kit/DOChildsComponentEditor")]
     public static void CreateDoChildsComponentEditor()
     {
-        string templatePath = "Assets/Plugins/DOTween Modular/Editor/Script Template/DOChildsComponentEditor.cs.txt";
+        string templatePath = "Assets/Plugins/Tween Kit/Editor/Script Template/DOChildsComponentEditor.cs.txt";
 
         ProjectWindowUtil.CreateScriptAssetFromTemplateFile(templatePath, "DOChildsComponentEditor.cs");
     }
